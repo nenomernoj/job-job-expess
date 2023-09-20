8a031c0e68cb82585f989b20e33f54d85034d259
Konstantin <ownedbycat@mail.ru>
2023-09-20 06:59:43 +0600

    fix

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const apiRoutes = require('./routes/api');
 const app = express();
-const port = 3000; // Порт, на котором будет работать сервер
+const port = 443;
+const options = {
+    key: fs.readFileSync('key.pem'),     // Путь к вашему ключу
+    cert: fs.readFileSync('cert.pem'),    // Путь к вашему сертификату
+    passphrase: '11111'
+};
 // Маршрут для обработки корневого URL
 app.get('/', (req, res) => {
     res.send('Привет, мир123!');
@@ -14,6 +21,11 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes); // Роуты для регистрации и аутентификации
 app.use('/api', apiRoutes);   // Роуты API для фронтенда
 
+https.createServer(options, app).listen(port, () => {
+    console.log(`Сервер Express.js запущен с SSL на порту ${port}`);
+});
+
+/*
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
-});
+});*/
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,87 @@
 // routes/auth.js
 const express = require('express');
+const axios = require('axios');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const connection = require('../db');
 const router = express.Router();
+router.post('/insert-sms', (req, res) => {
+    console.log('test');
+    const {phone_number, sms_code} = req.body;
+    if (!phone_number || phone_number.length !== 11 || !containsOnlyNumbers(phone_number)) {
+        res.status(500).json({error: 'Неверный номер телефона'});
+        return;
+    }
+    // Проверка наличия существующей записи с тем же номером телефона и временем менее 2 минут назад
+    const checkQuery = 'SELECT * FROM SMSVerification WHERE phone_number = ?';
+    connection.query(checkQuery, [phone_number], (err, rows) => {
+        console.log(rows);
+        if (rows.length > 0) {
+            const currentTime = new Date();
+            const sendDate = new Date(rows[0].added_time);
+            const timeDifference = currentTime - sendDate; // Разница в миллисекундах
+            console.log('cT:', currentTime, '2:', sendDate);
+            console.log(timeDifference);
+            if (timeDifference >= 2 * 60 * 1000) {
+                console.log('Прошло более 2 минут');
+                const deleteQuery = 'DELETE FROM SMSVerification WHERE phone_number = ?';
+                connection.query(deleteQuery, [phone_number], (err, result) => {
+                    if (err) {
+                        console.error('Ошибка при удалении записи: ' + err.message);
+                        res.status(500).json({error: 'Ошибка при удалении записи'});
+                        return;
+                    }
 
+                    // После удаления вставляем новую запись
+                    insertNewRecord();
+                });
+            } else {
+                res.status(500).json({error: 'Прошло менее 2 минут'});
+            }
+        } else {
+            // Если запись не существует, сразу вставляем новую запись
+            insertNewRecord();
+        }
+    });
+
+    // Функция для вставки новой записи
+    function insertNewRecord() {
+        const sms_code = Math.floor(Math.random() * 9000) + 1000;
+        const apiKey = 'kz0eeea0ff46739705065c0c7045f1edbf5d6a63ad5f58106936249e536af043c75037';
+        const recipient = phone_number;
+        const message = 'Ваш код регистрации: ' + sms_code;
+        axios.post(`https://api.mobizon.com/service/message/sendSmsMessage?output=json&api=v1&apiKey=${apiKey}`, {
+            from: 'Job Job',
+            recipient: recipient,
+            text: message,
+        })
+            .then((response) => {
+                console.log(response);
+                const insertQuery = 'INSERT INTO SMSVerification (phone_number, sms_code) VALUES (?, ?)';
+                connection.query(insertQuery, [phone_number, sms_code], (err, result) => {
+                    if (err) {
+                        console.error('Ошибка вставки записи: ' + err.message);
+                        res.status(500).json({error: 'Ошибка вставки записи'});
+                    } else {
+                        console.log('Запись успешно добавлена');
+                        res.status(200).json({message: 'Смс код выслан'});
+                    }
+                });
+            })
+            .catch((error) => {
+                console.error('Ошибка отправки SMS:', error);
+                res.status(500).json({error: 'Ошибка отправки SMS'});
+            });
+    }
+
+    function containsOnlyNumbers(inputString) {
+        // Используем регулярное выражение для проверки, что строка состоит только из цифр
+        const regex = /^[0-9]+$/;
+        return regex.test(inputString);
+    }
+});
 // Регистрация пользователя
-router.post('/register', async (req, res) => {
+router.post('/update-profile', async (req, res) => {
     try {
         console.log(req.body);
         const {photo, fullName, birthDate, gender, cityId, phoneNumber, email, password} = req.body;
